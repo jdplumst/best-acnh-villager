0bf35868ad7fd36f25aff573523d33b282a71527
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-12 00:34:22 -0500

    Style and modify the Home Page and Footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,17 +1,45 @@
 export default function Footer() {
   return (
-    <div className="flex items-center gap-5">
-      <a href="/rank">Rank</a>
+    <div className="flex items-center gap-2">
+      <a
+        href="/rank"
+        className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
+      >
+        Rank
+      </a>
       <span>·</span>
-      <a href="/leaderboard">Leaderboard</a>
+      <a
+        href="/leaderboard"
+        className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
+      >
+        Leaderboard
+      </a>
       <span>·</span>
-      <a href="/statistics">Statistics</a>
+      <a
+        href="/statistics"
+        className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
+      >
+        Statistics
+      </a>
       <span>·</span>
-      <a href="/villagers">Villagers</a>
+      <a
+        href="/villagers"
+        className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
+      >
+        Villagers
+      </a>
       <span>·</span>
-      <a href="https://github.com/jdplumst/best-acnh-villager">GitHub</a>
+      <a
+        href="https://github.com/jdplumst/best-acnh-villager"
+        className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
+      >
+        GitHub
+      </a>
       <span>·</span>
-      <a href="https://docs.google.com/spreadsheets/d/13d_LAJPlxMa_DubPTuirkIV4DERBMXbrWQsmSh8ReK4/edit#gid=400375391">
+      <a
+        href="https://docs.google.com/spreadsheets/d/13d_LAJPlxMa_DubPTuirkIV4DERBMXbrWQsmSh8ReK4/edit#gid=400375391"
+        className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
+      >
         Data Source
       </a>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,18 @@ import Footer from "./_components/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-b from-[#2e026d] to-[#15162c] px-4 py-16 text-white">
-      <h1 className="text-center text-5xl font-bold">
-        Who is the Best Animal Crossing: New Horizons Villager?
+      <h1 className=" text-center text-7xl font-bold">
+        Who is the Best{" "}
+        <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+          Animal Crossing: New Horizons
+        </span>{" "}
+        Villager?
       </h1>
       <a
         href="/rank"
-        className="rounded-full bg-purple-200 p-4 text-xl font-semibold text-black"
+        className="rounded-full bg-purple-200 p-4 text-xl font-semibold text-black hover:bg-purple-300"
       >
-        Click Here to Find Out
+        Click Here to Start Ranking
       </a>
       <Footer></Footer>
     </main>
