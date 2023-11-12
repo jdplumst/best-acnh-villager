62050ebae3f03bcafcd0c0c7d33928b390410015
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-12 03:10:09 -0500

    Make Footer look better on mobile

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,41 +1,41 @@
 export default function Footer() {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-x-2">
+    <div className="grid grid-cols-6 items-center justify-items-start gap-x-2 sm:flex sm:flex-wrap sm:items-center sm:justify-center">
       <a
         href="/rank"
         className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
       >
         Rank
       </a>
-      <span>·</span>
+      <span className="invisible sm:visible">·</span>
       <a
         href="/leaderboard"
         className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
       >
         Leaderboard
       </a>
-      <span>·</span>
+      <span className="invisible sm:visible">·</span>
       <a
         href="/statistics"
         className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
       >
         Statistics
       </a>
-      <span>·</span>
+      <span className="invisible sm:visible">·</span>
       <a
         href="/villagers"
         className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
       >
         Villagers
       </a>
-      <span>·</span>
+      <span className="invisible sm:visible">·</span>
       <a
         href="https://github.com/jdplumst/best-acnh-villager"
         className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
       >
         GitHub
       </a>
-      <span>·</span>
+      <span className="invisible sm:visible">·</span>
       <a
         href="https://docs.google.com/spreadsheets/d/13d_LAJPlxMa_DubPTuirkIV4DERBMXbrWQsmSh8ReK4/edit#gid=400375391"
         className="rounded-full p-4 hover:bg-purple-200 hover:text-black"
