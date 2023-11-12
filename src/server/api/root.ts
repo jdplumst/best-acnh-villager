4dad68c0c1919bd48dd31eaee9d4a6f5bf3de406
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-11 23:07:18 -0500

    Convert postRouter to villagerRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { villagerRouter } from "~/server/api/routers/villager";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  villager: villagerRouter,
 });
 
 // export type definition of API
