64682cbf78bc20cc40a9af7418c37385b005f101
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-14 17:26:09 -0500

    Add vote procedure

--- src/server/api/routers/villager.ts
+++ src/server/api/routers/villager.ts
@@ -1,4 +1,5 @@
-import { and, gte, ilike } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { and, eq, gte, ilike, or } from "drizzle-orm";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -38,10 +39,66 @@ export const villagerRouter = createTRPCRouter({
     const villagers = await ctx.db
       .select({ id: villager.id, name: villager.name, photo: villager.photo })
       .from(villager);
-    const villager1 = villagers[Math.floor(Math.random() * villagers.length)];
+    const villager1 = villagers[Math.floor(Math.random() * villagers.length)]!;
     const villager2 = villagers.filter((v) => v.id !== villager1?.id)[
       Math.floor(Math.random() * villagers.length - 1)
-    ];
+    ]!;
     return { villager1, villager2 };
   }),
+
+  vote: publicProcedure
+    .input(z.object({ voteForId: z.number(), voteAgainstId: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      const villagers = await ctx.db
+        .select({
+          id: villager.id,
+          votesFor: villager.votesFor,
+          votesAgainst: villager.votesAgainst,
+          rating: villager.rating,
+        })
+        .from(villager)
+        .where(
+          or(
+            eq(villager.id, input.voteForId),
+            eq(villager.id, input.voteAgainstId),
+          ),
+        );
+      if (villagers.length !== 2) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Must provide two distinct villager id's",
+        });
+      }
+      const villager1 = villagers.filter((v) => v.id === input.voteForId)[0]!;
+      const villager2 = villagers.filter(
+        (v) => v.id === input.voteAgainstId,
+      )[0]!;
+      const expected1 =
+        1 / (1 + 10 ** ((villager2.rating - villager1.rating) / 400));
+      const expected2 =
+        1 / (1 + 10 ** ((villager1.rating - villager2.rating) / 400));
+      const newRating1 = villager1.rating + 32 * (1 - expected1);
+      const newRating2 = villager1.rating + 32 * (0 - expected2);
+      try {
+        await ctx.db.transaction(async (tx) => {
+          await tx
+            .update(villager)
+            .set({ votesFor: villager1.votesFor + 1, rating: newRating1 })
+            .where(eq(villager.id, villager1.id));
+          await tx
+            .update(villager)
+            .set({
+              votesAgainst: villager2.votesAgainst + 1,
+              rating: newRating2,
+            })
+            .where(eq(villager.id, villager2.id));
+        });
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
+        });
+      }
+      return { message: "Vote success" };
+    }),
 });
